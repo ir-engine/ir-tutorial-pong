164923dd5850ace303e1f9822032b77133472af5
Anselm Hook <anselm@gmail.com>
2023-11-16 17:36:50 -0800

    slight cleanup to physics

--- src/components/PongComponent.ts
+++ src/components/PongComponent.ts
@@ -1,4 +1,4 @@
-import { defineComponent, removeComponent, useComponent, useOptionalComponent } from "@etherealengine/engine/src/ecs/functions/ComponentFunctions"
+import { defineComponent, getMutableComponent, removeComponent, useComponent, useOptionalComponent } from "@etherealengine/engine/src/ecs/functions/ComponentFunctions"
 import { useEntityContext } from "@etherealengine/engine/src/ecs/functions/EntityFunctions"
 import { useEffect } from "react"
 import { EntityUUID } from "@etherealengine/common/src/interfaces/EntityUUID"
@@ -6,6 +6,7 @@ import { CollisionComponent } from "@etherealengine/engine/src/physics/component
 import { matches } from "@etherealengine/engine/src/common/functions/MatchesUtils"
 import { UUIDComponent } from "@etherealengine/engine/src/scene/components/UUIDComponent"
 import { TextComponent } from "./TextComponent"
+import { Entity } from "@etherealengine/engine/src/ecs/classes/Entity"
 
 let collisions1 = 0
 let collisions2 = 0
@@ -55,32 +56,41 @@ export const PongComponent = defineComponent({
     // assert that the self entity has its own state attached to itself
     const entity = useEntityContext()
     const pong = useComponent(entity, PongComponent)
-/*
+
     // hit wall1?
     const wall1 = UUIDComponent.entitiesByUUID[pong.wall1.value]
-    const collision1 = useComponent(wall1, CollisionComponent )
+    const collision1 = useOptionalComponent(wall1, CollisionComponent )
+
+    // this is a way to detect a collision
+    // it is however not synchronous and in some use cases would be a problem
+    // in our case it is ok because we intend to reset the game state on a collision
+
     useEffect(() => {
-      removeComponent(wall1, CollisionComponent)
+      //removeComponent(wall1, CollisionComponent)
       const score1 = UUIDComponent.entitiesByUUID[pong.score1.value]
-      let text = useOptionalComponent(score1,TextComponent)
+      let text = getMutableComponent(score1,TextComponent)
       if(text) {
         text.text.set(`${collisions1++}`)
       }
-    }, [collision1.size])
+    }, [collision1])
 
     // hit wall2?
     const wall2 = UUIDComponent.entitiesByUUID[pong.wall2.value]
-    const collision2 = useComponent(wall2, CollisionComponent )
+    const collision2 = useOptionalComponent(wall2, CollisionComponent )
     useEffect(() => {
-      removeComponent(wall2, CollisionComponent)
+      //removeComponent(wall2, CollisionComponent)
       const score2 = UUIDComponent.entitiesByUUID[pong.score1.value]
-      let text = useOptionalComponent(score2,TextComponent)
+      let text = getMutableComponent(score2,TextComponent)
       if(text) {
         text.text.set(`${collisions2++}`)
       }
-    }, [collision2.size])
-  */  
+    }, [collision2])
+
     return null
   }
 })
+
+function useMutableComponent(score1: Entity, TextComponent: { name: string; jsonID: string; onInit: (this: import("bitecs").ComponentType<Record<string, any>>, entity: import("@etherealengine/engine/src/ecs/classes/Entity").Entity) => { text: string; geometry: import("@etherealengine/engine/src/assets/constants/Geometry").Geometry; mesh: import("three").Mesh<import("three").BufferGeometry<import("three").NormalBufferAttributes>, import("three").Material | import("three").Material[], import("three").Object3DEventMap> }; toJSON: (entity: import("@etherealengine/engine/src/ecs/classes/Entity").Entity, component: import("@hookstate/core").State<{ text: string; geometry: import("@etherealengine/engine/src/assets/constants/Geometry").Geometry; mesh: import("three").Mesh<import("three").BufferGeometry<import("three").NormalBufferAttributes>, import("three").Material | import("three").Material[], import("three").Object3DEventMap> }>) => { text: string }; onSet: (entity: import("@etherealengine/engine/src/ecs/classes/Entity").Entity, component: import("@hookstate/core").State<{ text: string; geometry: import("@etherealengine/engine/src/assets/constants/Geometry").Geometry; mesh: import("three").Mesh<import("three").BufferGeometry<import("three").NormalBufferAttributes>, import("three").Material | import("three").Material[], import("three").Object3DEventMap> }>, json: Partial<{ text: string }> | undefined) => void; onRemove: (entity: import("@etherealengine/engine/src/ecs/classes/Entity").Entity, component: import("@hookstate/core").State<{ text: string; geometry: import("@etherealengine/engine/src/assets/constants/Geometry").Geometry; mesh: import("three").Mesh<import("three").BufferGeometry<import("three").NormalBufferAttributes>, import("three").Material | import("three").Material[], import("three").Object3DEventMap> }>) => void; reactor: () => null } & import("bitecs").ComponentType<Record<string, any>> & import("@etherealengine/engine/src/ecs/functions/ComponentFunctions").Component<{ text: string; geometry: import("@etherealengine/engine/src/assets/constants/Geometry").Geometry; mesh: import("three").Mesh<import("three").BufferGeometry<import("three").NormalBufferAttributes>, import("three").Material | import("three").Material[], import("three").Object3DEventMap> }, Record<string, any>, { text: string }, Partial<{ text: string }>, ""> & { _TYPE: { text: string; geometry: import("@etherealengine/engine/src/assets/constants/Geometry").Geometry; mesh: import("three").Mesh<import("three").BufferGeometry<import("three").NormalBufferAttributes>, import("three").Material | import("three").Material[], import("three").Object3DEventMap> } }) {
+  throw new Error("Function not implemented.")
+}
 
--- src/systems/PongSystem.ts
+++ src/systems/PongSystem.ts
@@ -11,7 +11,6 @@ import { Entity } from "@etherealengine/engine/src/ecs/classes/Entity";
 import { TextComponent } from "../components/TextComponent";
 
 const query1 = defineQuery([PongComponent])
-const query2 = defineQuery([CollisionComponent])
 
 let collisions1 = 0
 let collisions2 = 0
@@ -41,24 +40,17 @@ export const PongSystem = defineSystem({
       let score1text = getMutableComponent(score1,TextComponent)
       let score2text = getMutableComponent(score2,TextComponent)
 
-      // monstrously brute force search for collisions related to this game
-      for (const collisionEntity of query2()) {
-        if(collisionEntity == wall1) {
-          score1text.text.set(`${collisions1++}`)
-          //updateComponent(score1,TextComponent,{text:`${collisions1++}`})
-        }
-        if(collisionEntity == wall2) {
-          score2text.text.set(`${collisions2++}`)
-          //updateComponent(score2,TextComponent,{text:`${collisions2++}`})
-        }
+      // @todo check for enter specifically, and perhaps make sure it is the ball!
+      let c1 = getComponent(wall1,CollisionComponent)
+      if(c1) {
+        removeComponent(wall1, CollisionComponent)
+        score1text.text.set(`${collisions1++}`)
       }
 
-      // flush any collisions
-      try {
-        removeComponent(wall1, CollisionComponent)
+      let c2 = getComponent(wall2,CollisionComponent)
+      if(c2) {
         removeComponent(wall2, CollisionComponent)
-      } catch(err) {
-        console.error(err)
+        score2text.text.set(`${collisions2++}`)
       }
 
     }
