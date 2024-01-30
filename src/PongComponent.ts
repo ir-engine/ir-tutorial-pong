09639374999fc23963c3e06a4e2897b3aa89030d
HexaField <joshfield999@gmail.com>
2024-01-30 14:15:35 +1100

    fix imports

--- src/PongComponent.ts
+++ src/PongComponent.ts
@@ -4,7 +4,7 @@ import { dispatchAction, getState } from '@etherealengine/hyperflux'
 import { useEffect } from 'react'
 import { PongActions } from './PongGameSystem'
 import { defineComponent, useEntityContext, getComponent } from '@etherealengine/ecs'
-import { EngineState } from '@etherealengine/engine/src/EngineState'
+import { EngineState } from '@etherealengine/spatial/src/EngineState'
 
 export const PongComponent = defineComponent({
   name: 'PongComponent',
